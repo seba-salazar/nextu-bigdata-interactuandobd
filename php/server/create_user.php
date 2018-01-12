4eb38bf77d9c49c7f05dcced909f8701ac85df40
Sebastian Salazar <sebastian.salazar@agrosat.cl>
2018-01-12 11:45:38 -0300

    1ra carga de archivos...

---
+++ php/server/create_user.php
@@ -0,0 +1,49 @@
+<?php
+	require('conector.php');
+	$con = new conectorBD();
+
+	$response['conexion'] = $con->initConexion($con->database);
+	if ($response['conexion'] == 'OK'){
+		$conexion = $con->getConexion();
+		$insert = $conexion->prepare('INSERT INTO usuarios (email, nombre, password , fecha_nacimiento) VALUES (?,?,?,?)');
+		$insert->bind_param("ssss", $email, $nombre, $password, $fecha_nacimiento);
+
+		$d_password = "1234";
+		$email = "sebastian@mail.com";
+		$nombre = "sebastian";
+		$password = password_hash($d_password, PASSWORD_DEFAULT);
+		$fecha_nacimiento = "1982-10-08";
+
+		$insert->execute();
+
+		$d_password = "1234";
+		$email = "daniela@mail.com";
+		$nombre = "daniela";
+		$password = password_hash($d_password, PASSWORD_DEFAULT);
+		$fecha_nacimiento = "1982-10-07";
+
+		$insert->execute();
+
+		$d_password = "1234";
+		$email = "christian@mail.com";
+		$nombre = "christian";
+		$password = password_hash($d_password, PASSWORD_DEFAULT);
+		$fecha_nacimiento = "1983-09-23";
+
+		$insert->execute();
+
+		$response['resultado'] = "1";
+		$response['msg']= 'Informacion de inicio:';
+		$getUsers = $con->consultar(['usuarios'],['*'],$condicion = "");
+		while ($fila= $getUsers->fetch_assoc()) {
+			$response['msg'].=$fila['email'];
+		}
+		$response['msg'].= 'contraseña: '.$d_password;
+		}else{
+			$response['resultado'] == "0";
+			$response['msg'] = 'No se pudo conectar a la base de datos';
+		}
+
+		echo json_encode($response);
+
+ ?>
